f496df0645aa0c4c18568d60d0e0270ed2c0d85b
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2023-06-09 13:51:25 +0530

    added event listener to display marks

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ doc = document.getElementById("choice");
 function handleEvent(event) {
     if (event.target=== doc) {
         val = doc.value;
+        var catex = document.getElementById("cat-select");
+        var assignm = document.querySelector(".assign-select");
         if (val==1) {
-            var catex = document.getElementById("cat-select");
             catex.classList.remove("hidden");
-            var assignm = document.querySelector(".assign-select");
             assignm.classList.add("hidden")
-
-            
         }
 
         else if (val==2) {
-            var catex = document.getElementById("cat-select");
             catex.classList.add("hidden");
-            var assignm = document.querySelector(".assign-select");
             assignm.classList.remove("hidden")
         }
+
+
+        var result = document.querySelector(".marklist");
+
+        document.addEventListener("click", function(e) {
+            var b = document.querySelector("#cat-ch");
+            var c = document.querySelector("#assign-ch");
+            if (e.target === b || e.target === c) {
+                result.classList.remove("hidden");
+            }
+        });
+
+
     }
+
+
 }
 
 document.addEventListener("click", handleEvent)
