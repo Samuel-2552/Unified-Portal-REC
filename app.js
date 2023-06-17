0cd2d03c1f6e657024939f291480c80ba951dca6
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2023-06-18 01:35:44 +0530

    refactored post request, and formatted response.

--- app.js
+++ app.js
@@ -3,33 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
 
-const stu_details = JSON.parse(fs.readFileSync('stu_data.json', 'utf-8'));
+const parseData = require("./parseData");
+const { parse } = require('path');
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
-async function getDetails(rollNo) {
-    const number = stu_details[rollNo][0];
-
-    const data = await fetch("http://rajalakshmi.in/UI/Modules/HRMS/ManageStaffStudent/UniPersonInfo.asmx/BindInternalMarks", {
-        "headers": {
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "content-type": "application/json; charset=UTF-8",
-            "x-requested-with": "XMLHttpRequest",
-            "cookie": "G_ENABLED_IDPS=google; ASP.NET_SessionId=gph3kir0ocdmqulpsgf2nbuw; dcjq-accordion=10%2C12",
-            "Referer": "http://rajalakshmi.in/UI/Modules/Profile/Profile.aspx?FormHeading=myProfile",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
-        },
-        "body": "{\"PersonId\":" + number + ",\"Semester\":4,\"Category\":0}", //filter based on semester, tho, idk waht the category thing does, till now
-        "method": "POST"
-    });
-
-    return data.text();
-
-}
 
 var body = ''
 
@@ -43,11 +23,13 @@ var rollNumber = '';
 
 app.post("/", function(req, res) {
     rollNumber = req.body.rollnumber;
-    
-    getDetails(rollNumber).then((data) => {
-        body = data;
+
+    parseData(rollNumber).then(function(data) {
+        body = JSON.stringify(data);
         res.redirect("/details");
-    });
+    })
+
+    
     
 
 });
---
+++ parseData.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const stu_details = JSON.parse(fs.readFileSync('stu_data.json', 'utf-8'));
+
+async function getDetails(rollNo) {
+    const number = stu_details[rollNo][0];
+
+    const data = await fetch("http://rajalakshmi.in/UI/Modules/HRMS/ManageStaffStudent/UniPersonInfo.asmx/BindInternalMarks", {
+        "headers": {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "content-type": "application/json; charset=UTF-8",
+            "x-requested-with": "XMLHttpRequest",
+            "cookie": "G_ENABLED_IDPS=google; ASP.NET_SessionId=gph3kir0ocdmqulpsgf2nbuw; dcjq-accordion=10%2C12",
+            "Referer": "http://rajalakshmi.in/UI/Modules/Profile/Profile.aspx?FormHeading=myProfile",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": "{\"PersonId\":" + number + ",\"Semester\":4,\"Category\":0}", //filter based on semester, tho, idk waht the category thing does, till now
+        "method": "POST"
+    });
+
+    return data.text();
+
+}
+
+function parseData(rollNo) {
+    return getDetails(rollNo).then(function (data) {
+        data = JSON.parse(data);
+        const xyz = JSON.parse(data.d);
+        return xyz;
+    });
+}
+module.exports = parseData;
