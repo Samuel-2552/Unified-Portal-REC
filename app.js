e4b1db832a196388295c0783200bc3debf5b5dd1
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2023-06-23 10:58:12 +0530

    basic session implementation

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
+const session = require('express-session');
+const uuid = require('uuid');
 
 const parseData = require("./parseData");
 
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+    genid: function(req) {
+        return uuid.v4();
+    },
+    secret: 'random-secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 var response = ''
@@ -26,9 +37,28 @@ app.post("/", function(req, res) {
 });
 
 
+app.use(function(req, res, next) {
+    if (!req.session.initialized) {
+        req.session.initialized = true;
+
+        return res.redirect('/');
+    }
+
+    next();
+});
+
+
 
 app.get("/details", function(req, res) {
-    res.sendFile(__dirname + "/views/selection.html");
+
+    console.log(req.session.initialized);
+
+    if (req.session.initialized) {
+        res.sendFile(__dirname + "/views/selection.html");
+    }
+    else {
+        res.redirect("/");
+    }
 });
 
 app.post("/details", function(req, res) {
@@ -40,6 +70,7 @@ app.post("/details", function(req, res) {
         const cat = req.body.catNumber;
 
         parseData(rollNumber, semester, choice, cat).then(function (data) {
+            //temporary result. 
             let toBeSent = '<table>';
             toBeSent += "<tr><th>Subject</th><th>U1</th><th>U2</th><th>U3</th><th>U4</th><th>U5</th><th>Total</th></tr>"
             data.forEach(function (obj) {
